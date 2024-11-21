5d4bc2cd5f03cfcdf75f02f728ccb9bdfa00ff44
Maya Burgess <mayaburgess@Mayas-MacBook-Pro-3.local>
2024-11-20 23:53:55 -0800

    Final tidy. Can't hide :)

--- scripts/Maya_Burgess_Assignment_8.R
+++ scripts/Maya_Burgess_Assignment_8.R
@@ -21,31 +21,28 @@ library(lubridate)
 mloa <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
 
 mloa2 = mloa %>%
-  # Remove NA's
   filter(rel_humid != -99) %>%
   filter(temp_C_2m != -999.9) %>%
   filter(windSpeed_m_s != -999.9) %>%
-  # Create datetime column (README indicates time is in UTC)
   mutate(datetime = ymd_hm(paste0(year,"-", 
                                   month, "-", 
                                   day," ", 
                                   hour24, ":", 
                                   min), 
                            tz = "UTC")) %>%
-  # Convert to local time
   mutate(datetimeLocal = with_tz(datetime, tz = "Pacific/Honolulu"))
 
 ##plot (ish) - needed a lot of help here. Thank you!!
+
 mloa2 %>%
   mutate(localMon = month(datetimeLocal, label = TRUE),
          localHour = hour(datetimeLocal)) %>%
   group_by(localMon, localHour) %>%
   summarize(meantemp = mean(temp_C_2m)) %>%
-  # Plot
   ggplot(aes(x = localMon,
              y = meantemp)) +
   geom_point(aes(col = localHour)) +
-  # Use a nice color ramp - I tried them all and I like this one best, sorry!
+  # I tried them all and I like this one best, sorry!
   scale_color_viridis_c() +
   xlab("Month") +
   ylab("Mean temperature (degrees C)") +
